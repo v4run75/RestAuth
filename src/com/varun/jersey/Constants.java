fe1fb2c2a570d7c20182ae2d21f10d437b581369
v4run75 <varunkumar_95@yahoo.com>
2018-08-14 18:50:42 +0530

    Add files via upload

---
+++ src/com/varun/jersey/Constants.java
@@ -0,0 +1,9 @@
+package com.varun.jersey;
+
+public class Constants {
+	public static String dbClass = "com.mysql.jdbc.Driver";
+	private static String dbName= "u663670234_rest";
+	public static String dbUrl = "jdbc:mysql//mysql.hostinger.in/"+dbName;
+	public static String dbUser = "u663670234_rest";
+	public static String dbPwd = "Letsrock75";
+}
